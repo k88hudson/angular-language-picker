99fb780faa3e71fe9eebd9326d3fa6001d0c9599
Kate Hudson <k88hudson@gmail.com>
2014-07-24 18:09:24 -0400

    added modal to directive

--- dist/angular-language-picker.js
+++ dist/angular-language-picker.js
@@ -1,5 +1,5 @@
 angular
-  .module('k8LanguagePicker', ['templates-k8LanguagePicker'])
+  .module('k8LanguagePicker', ['templates-k8LanguagePicker', 'ui.bootstrap'])
   .constant('langMap', {
     'ach': {
       nativeName: "Lwo",
@@ -727,25 +727,40 @@ angular
     }
   })
   .directive('langPicker', [
+    '$modal',
     'langMap',
-    function (langMap) {
+    function ($modal, langMap) {
       return {
         restrict: 'AE',
+        transclude: true,
         scope: {
           supportedLanguages: '=',
-          onLanguageChange: '&'
+          callback: '&onLanguageChange'
         },
-        templateUrl: 'lang-picker.html',
+        replace: true,
+        templateUrl: 'lang-picker-button.html',
         link: function (scope, el, attrs, ctrl) {
-          scope.limit = 24;
-          scope.langInfo = scope.supportedLanguages.map(function (lang) {
-            var obj = langMap[lang] || {
-              nativeName: lang,
-              englishName: lang
-            };
-            obj.lang = lang;
-            return obj;
-          });
+          scope.open = function() {
+            $modal.open({
+              templateUrl: 'lang-picker.html',
+              controller: function($scope, $modalInstance) {
+                $scope.limit = 24;
+                $scope.supportedLanguages = scope.supportedLanguages;
+                $scope.langInfo = $scope.supportedLanguages.map(function (lang) {
+                  var obj = langMap[lang] || {
+                    nativeName: lang,
+                    englishName: lang
+                  };
+                  obj.lang = lang;
+                  return obj;
+                });
+                $scope.onLanguageChange = function(lang) {
+                  scope.callback(lang);
+                  $modalInstance.close();
+                };
+              }
+            });
+          };
         }
       };
     }
--- dist/angular-language-picker.templates.js
+++ dist/angular-language-picker.templates.js
@@ -1,29 +1,10 @@
-angular.module('templates-k8LanguagePicker', ['lang-picker-modal.html', 'lang-picker.html']);
+angular.module('templates-k8LanguagePicker', ['lang-picker-button.html', 'lang-picker.html']);
 
-angular.module("lang-picker-modal.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("lang-picker-modal.html",
-    "<div class=\"modal-body\">\n" +
-    "  <h3>Choose a language</h3>\n" +
-    "  <div class=\"form-group\">\n" +
-    "    <input class=\"form-control\" type=\"search\" ng-model=\"langSearch\">\n" +
-    "  </div>\n" +
-    "\n" +
-    "  <p>Webmaker is offered in 18 languages</p>\n" +
-    "  <div class=\"row\">\n" +
-    "    <div class=\"col-xs-6 col-sm-4 col-lg-3\" ng-repeat=\"lang in langInfo | filter:langSearch | limitTo: limit\">\n" +
-    "      <a href=\"#\" ng-click=\"onLanguageChange({lang: lang.lang})\" class=\"ellipsis\">\n" +
-    "        {{lang.nativeName}}\n" +
-    "      </a>\n" +
-    "    </div>\n" +
-    "    <div ng-if=\"langInfo.length - limit > 0\" class=\"col-xs-6 col-sm-3 col-lg-2\">\n" +
-    "      + {{langInfo.length - limit}} more\n" +
-    "    </div>\n" +
-    "  </div>\n" +
-    "</div>\n" +
-    "\n" +
-    "<div class=\"modal-footer\">\n" +
-    "  Don't see your language? <a href=\"\">Help us translate!</a>\n" +
-    "</div>\n" +
+angular.module("lang-picker-button.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("lang-picker-button.html",
+    "<button ng-click=\"open()\" class=\"btn btn-default\">\n" +
+    "  <span ng-transclude></span>\n" +
+    "</button>\n" +
     "");
 }]);
 
@@ -32,7 +13,7 @@ angular.module("lang-picker.html", []).run(["$templateCache", function($template
     "<div class=\"modal-body\">\n" +
     "  <h3>Choose a language</h3>\n" +
     "  <div class=\"form-group\">\n" +
-    "    <input class=\"form-control\" type=\"search\" ng-model=\"langSearch\">\n" +
+    "    <input class=\"form-control\" type=\"search\" ng-model=\"langSearch\" placeholder=\"Search languages...\">\n" +
     "  </div>\n" +
     "\n" +
     "  <p>Webmaker is offered in 18 languages</p>\n" +
